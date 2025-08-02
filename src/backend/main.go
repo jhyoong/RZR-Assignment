b2d9adf4c099cb42ee3f335713e939f1f111f437
JiaHui <yoongjh95@gmail.com>
2025-08-02 17:15:14 +0800

    add nginx reverse proxy

--- src/backend/main.go
+++ src/backend/main.go
@@ -65,7 +65,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Real-IP, X-Forwarded-For")
 		
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
